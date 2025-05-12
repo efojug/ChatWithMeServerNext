b3ddf7f176f1b31e453df65f85e0deeeafa3f79c
efojug <lidudu890890@163.com>
2025-05-12 18:29:42 +0800

    init project

---
+++ src/main/kotlin/com/efojug/chatwithmeservernext/ChatWithMeServerNextApplication.kt
@@ -0,0 +1,11 @@
+package com.efojug.chatwithmeservernext
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatWithMeServerNextApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatWithMeServerNextApplication>(*args)
+}
---
+++ src/test/kotlin/com/efojug/chatwithmeservernext/ChatWithMeServerNextApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.efojug.chatwithmeservernext
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatWithMeServerNextApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
